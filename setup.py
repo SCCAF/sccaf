1c2c871da2ece2dbad5930625bf9ee8363c673a0
Zhichao (Chichau) Miao <chichaumiau@gmail.com>
2020-04-25 22:47:12 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
             'scikit-learn',
             'psutil',
             'scanpy==1.4.6'],
-        scripts=['cli/sccaf', 'cli/sccaf-assess', 'cli/sccaf-assess-merger'],
+        scripts=['cli/sccaf', 'cli/sccaf-assess', 'cli/sccaf-assess-merger', 'cli/sccaf-regress-out'],
         author='Chichau Miau',
         author_email='zmiao@ebi.ac.uk',
         license='MIT'
