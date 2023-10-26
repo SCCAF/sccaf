50efca66fa2cfc1a6c45ad4fa88f5c6f239586c3
YY-SONG0718 <794815059@qq.com>
2023-10-26 18:11:17 +0100

    update scanpy version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ print(find_packages())
 
 setup(
         name='SCCAF',
-        version='0.0.10',
+        version='0.0.11',
         description='Single-Cell Clustering Assessment Framework',
         long_description=readme(),
         long_description_content_type='text/markdown',
@@ -20,9 +20,10 @@ setup(
             'louvain',
             'scikit-learn',
             'psutil',
-            'scanpy==1.4.6'],
+            'scanpy==1.9.2'],
         scripts=['cli/sccaf', 'cli/sccaf-assess', 'cli/sccaf-assess-merger', 'cli/sccaf-regress-out'],
         author='Chichau Miau',
         author_email='zmiao@ebi.ac.uk',
         license='MIT'
     )
+# modified by Yuyao Song <ysong@ebi.ac.uk> on Oct 2023 to bump scanpy version
